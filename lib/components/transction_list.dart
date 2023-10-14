e1e03eff36fe5c901bf95d1f36c0d5559b1eb01b
Bene-Tesla <benevaniosantos930@gmail.com>
2023-10-14 12:07:27 -0300

    :sparkles: adicionando um layout builder para alguns componentes

--- lib/components/transction_list.dart
+++ lib/components/transction_list.dart
@@ -12,23 +12,24 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: [
-              const Text(
-                'Nenhuma transação Cadastrada!',
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              Container(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
-          )
+        ? LayoutBuilder(
+            builder: (context, constraints) => Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Text(
+                      'Nenhuma Transação Cadastrada!',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: constraints.maxHeight * .6,
+                      child: Image.asset(
+                        'assets/images/waiting.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ))
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
